618b7ade37fc17f6fb036e592f070283282ff12e
SUPERustam <498rustam@gmail.com>
2022-03-11 19:27:41 +0300

    🙍 New script to connect telegram to your server

--- wakeup_telegram_bot.py
+++ wakeup_telegram_bot.py
@@ -5,10 +5,11 @@ import httpx
 
 TOKEN = os.getenv("TOKEN")
 SERVER_URL = os.getenv("SERVER_URL")
-TELEGRAM_URL = f"https://api.telegram.org/bot{TOKEN}/"
 
+TELEGRAM_URL = f"https://api.telegram.org/bot{TOKEN}/"
 response = httpx.get(TELEGRAM_URL + "setWebhook",
-                     params={"url": f"https://jxt3e9.deta.dev/telegram_bot"})
-response = httpx.get(TELEGRAM_URL+"getWebhookInfo")
-print(response.url)
+                     params={"url": SERVER_URL})
+response = httpx.get(TELEGRAM_URL + "getWebhookInfo")
+
+pprint(response.url)
 pprint(response.text)
